7a6714ef9294fbb5fa6d27cd0dce44feaf9d639c
Catauuu <catadomianm@gmail.com>
2026-05-18 11:28:20 -0300

    Add files via upload

---
+++ b.py
@@ -0,0 +1,12 @@
+num 1 = int (input ("nota 1"))
+num 1 = int (input ("nota 2"))
+num 1 = int (input ("nota 3"))
+promedio = (num1 + num2 + num3) / 3
+if (promedio <1 or promedio <10):
+    print ("error")
+elif (promedio >=1 and promedio <6):
+    print ("en proceso")
+elif (promedio >=6 and promedio <8):
+    print ("en proceso")
+elif (promedio >=8 and promedio <10):
+    print ("en proceso")
